7bdf17fc4d86fb5974c4d23f28cb763fabff1b1e
Geoff Williams <geoff.williams@puppetlabs.com>
2016-09-19 12:34:52 +1000

    error handling kinda

--- lib/facter/network_address.rb
+++ lib/facter/network_address.rb
@@ -2,10 +2,13 @@ require 'socket'
 Facter.add(:source_ipaddress) do
   setcode do
     # read the target network address
-    target = File.read('/etc/target_ip').strip
+    begin
+      target = File.read('/etc/target_ip').strip
 
-    # http://stackoverflow.com/a/5030118
-    UDPSocket.open {|s| s.connect(target, 1); s.addr.last}
-
+      # http://stackoverflow.com/a/5030118
+      UDPSocket.open {|s| s.connect(target, 1); s.addr.last}
+    rescue e
+      "ERROR:  #{e.message}"
+    end
   end
 end
