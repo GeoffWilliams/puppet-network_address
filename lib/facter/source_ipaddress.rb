eda11ef2deb38ea3aa231439b508a555d1fdb8b8
Geoff Williams <geoff.williams@puppetlabs.com>
2016-09-24 19:10:04 +1000

    fixup broken fact

--- lib/facter/source_ipaddress.rb
+++ lib/facter/source_ipaddress.rb
@@ -2,19 +2,21 @@ require 'socket'
 Facter.add(:source_ipaddress) do
   setcode do
     # read the target network address
+    v = {}
     begin
-      v = {}
       File.readlines('/etc/target_ip').each do |line|
         host = line.strip
         # http://stackoverflow.com/a/5030118
         begin
-          v[host] = UDPSocket.open {|s| s.connect(target, 1); s.addr.last} 
+          v[host] = UDPSocket.open {|s| s.connect(host, 1); s.addr.last} 
         rescue Exception => e
           v[host] = "ERROR:  #{e.message}"
         end
       end
     rescue Exception => e
-      "ERROR:  #{e.message}"
+      v['ERROR'] = e.message
     end
+
+    v
   end
 end
