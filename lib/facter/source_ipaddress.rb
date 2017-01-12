f7336369f1356cb9fc4a7292e488ac7427238084
Geoff Williams <geoff.williams@puppetlabs.com>
2017-01-13 09:22:38 +1100

    use pluginsync text file for targets to avoid needing multiple runs

--- lib/facter/source_ipaddress.rb
+++ lib/facter/source_ipaddress.rb
@@ -4,17 +4,16 @@ Facter.add(:source_ipaddress) do
   setcode do
     # read the target network address
     v = {}
-    if Gem.win_platform?
-      target_file = 'c:\Users\Administrator\AppData\Roaming\target_ip'
-    else
-      target_file = '/etc/target_ip'
-    end
+
+    # read the list of IP address targets, distributed from your roles and
+    # profiles via pluginsync
+    target_file = File.join(Puppet[:pluginfactdest], 'source_ipaddress_targets.txt')
     begin
       File.readlines(target_file).each do |line|
         begin
           host = line.strip
           # http://stackoverflow.com/a/5030118
-          v[host] = UDPSocket.open {|s| s.connect(host, 1); s.addr.last} 
+          v[host] = UDPSocket.open {|s| s.connect(host, 1); s.addr.last}
         rescue Exception => e
           # connection errors per host
           v[host] = "ERROR:  #{e.message}"
